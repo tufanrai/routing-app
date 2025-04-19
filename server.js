f34fb50780354666ace765c30c511671d996194f
TufanRai <thulungtufan16@gmail.com>
2025-04-19 08:18:43 +0545

    Added some comments to understand which code is performing what functionality

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const app = express()
+const app = express()  // app instant
 
-app.get('/', (req,res) => {
-    res.send('this is a landing page')
+app.get('/', (req,res) => { 
+    res.send('this is a landing page') //responce to the request ofthe user in the home page
 })
 
 app.get('/users', (req,res) => {
-    res.send('this is a users page')
+    res.send('this is a users page') // responce to the request of the user  in the users page
 })
 
 app.get('/users/:id', (req,res) => {
-    res.send(`this is a page of user with id ${req.params.id}`)
+    res.send(`this is a page of user with id ${req.params.id}`) // responce to the request of the user in the selective id page
 })
 
 app.listen(8000, () => console.log('server started at http://localhost:8000'))
